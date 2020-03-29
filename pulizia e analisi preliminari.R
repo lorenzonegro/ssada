e0d4819be9170cd947fbfa28098068beaa9eece8
Lorenzo Negro <lorenzo.negro@studenti.unipd.it>
2020-03-29 14:13:36 +0200

    pulizia

---
+++ pulizia e analisi preliminari.R
@@ -0,0 +1,9 @@
+#Pulizia e analisi preliminari dataset
+rm(list=ls())
+setwd("C:/Users/user1/Desktop/UNIVERSITA'/DATI AZIENDALI/googleplaystore")
+gps=read.csv("googleplaystore.csv")
+gpsr=read.csv("googleplaystore_user_reviews.csv")
+head(gps)
+str(gps)
+hist(gps$Rating,xlim=c(0,5),nclass=100)
+mean(gps$Rating)
