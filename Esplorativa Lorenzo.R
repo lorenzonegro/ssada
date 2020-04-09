0939bb9817f4e217116c71046434e9879e0abb00
Lorenzo Negro <lorenzo.negro@studenti.unipd.it>
2020-04-09 12:10:36 +0200

    Inizio esplorativa

--- Esplorativa Lorenzo.R
+++ Esplorativa Lorenzo.R
@@ -16,7 +16,7 @@ table(google_app$Category[google_app$Installs>="[50.000.000, 100.000.000)"])
 #Generi più presenti
 top20=sort(table(google_app$Category),decreasing = T)[1:20]
 nomi21=c(names(top20),"Others")
-others.dim=dim(google_app)[1]-sum(as.numeric(top10))
+others.dim=dim(google_app)[1]-sum(as.numeric(top20))
 
 top21=c(as.numeric(top20),others.dim)
 names(top21)=nomi21
@@ -26,16 +26,3 @@ pie(as.numeric(top21),nomi21, col=hcl.colors(10,"Greens"),clockwise=T,
     main="FlowingData Pool",lty=0, cex=0.7)
 
 
-
-#Generi con più reviews
-
-top20=sort(table(google_app$Category),decreasing = T)[1:20]
-nomi21=c(names(top20),"Others")
-others.dim=dim(google_app)[1]-sum(as.numeric(top10))
-
-top21=c(as.numeric(top20),others.dim)
-names(top21)=nomi21
-
-pie(as.numeric(top21),nomi21)
-pie(as.numeric(top21),nomi21, col=hcl.colors(10,"Greens"),clockwise=T,
-    main="FlowingData Pool",lty=0, cex=0.7)
