fe90c0da1178a54514097e9957c47c1efeeb7f55
Lorenzo Negro <lorenzo.negro@studenti.unipd.it>
2020-04-25 17:37:31 +0200

    COnfronto tra varia o meno device

--- Esplorativa Lorenzo.R
+++ Esplorativa Lorenzo.R
@@ -25,4 +25,25 @@ pie(as.numeric(top21),nomi21)
 pie(as.numeric(top21),nomi21, col=hcl.colors(10,"Greens"),clockwise=T,
     main="FlowingData Pool",lty=0, cex=0.7)
 
-length(which(google_app$Category==google_app$Genres))
+#Implemento ciò che ha scritto Dani con le mie variabili
+
+library(ggplot2)
+library(lubridate)
+countInst <- as.data.frame(table(google_app$Installs))
+
+#Reviews
+mu.rev=median(google_app$Reviews)
+ggplot(google_app[google_app$Reviews<=mu.rev,]) + 
+  geom_bar(aes(y=Installs, x = (..count..)/sum(..count..)), position="dodge")
+ggplot(google_app[google_app$Reviews>mu.rev,]) + 
+  geom_bar(aes(y=Installs, x = (..count..)/sum(..count..)), position="dodge")
+
+#Per categoria e genere dobbiamo capire quali e in che modo indagare. MACRO CATEGORIE?
+
+#Android ver
+#Modifica confrontando relative device fissi e device variati
+
+ggplot(google_app) + geom_bar(aes(x=Android.Ver, fill=Installs), position="dodge")
+#Content rating
+ggplot(google_app) + geom_bar(aes(x=Content.Rating, fill=Installs), position="dodge")
+
