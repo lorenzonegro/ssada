9fd532397ff0f3a57c618ae40685d18685ea5b79
‘daniGiro’ <daniele96.g@gmail.com>
2020-04-26 16:23:01 +0200

    dani

--- esplorazione daniele.R
+++ esplorazione daniele.R
@@ -10,6 +10,8 @@ ggplot(google_app) + geom_bar(aes(y=Installs))
 
 
 #### Type_Price #### 
+ggplot(google_app) + geom_bar(aes(x=Type_Price, y=(..count..)/sum(..count..), fill=Installs), position="dodge")
+
 ty_free <- ggplot(google_app[google_app$Type_Price=="Free",]) + 
   geom_bar( aes(x=Installs, y = (..count..)/sum(..count..)), position="dodge", fill="#00b200") + 
   geom_density(aes(x = as.numeric(Installs)),adjust=1.75, col="#004900") + ggtitle("Gratis")+
@@ -36,10 +38,11 @@ grid.arrange(ty_free,ty_paid,
 ggplot(google_app) + geom_boxplot(aes(y=Size, x=Installs, fill=Installs))+
   theme(legend.position = "none")
 sum(is.na(google_app$Size))
+table(google_app$Installs[is.na(google_app$Size)])
 #ATTENZIONE CI SONO NA!!!! eliminiamo le righe??
 
 #Price
-prova <- which(google_app$Price>0 & google_app$Price<25)
+prova <- which(google_app$Price>0 & google_app$Price<100)
 ggplot(google_app[prova,]) + geom_boxplot(aes(y=Price, x=Installs, fill=Installs)) + coord_cartesian(ylim = c(0,20))
 
 ggplot(google_app[prova,]) + geom_density(aes(x=Price, col=Installs))
@@ -49,7 +52,7 @@ google_app[google_app$Price>100,]
 
 
 
-ggplot(google_app) + geom_density(aes(col=Installs,x=as.character(year(Last.Updated))), position="dodge")
+ggplot(google_app) + geom_bar(aes(fill=Installs,x=as.character(year(Last.Updated))), position="dodge")
 
 
 #### Last update #### 
@@ -66,6 +69,7 @@ ty_20xx <- ggplot(google_app[year(google_app$Last.Updated)<2018,]) +
   theme(axis.title = element_blank(), plot.title = element_text(size=11, face="bold"))+
   geom_density(aes(x = as.numeric(Installs)),adjust=1.5, col = "#923c1a")
 
+
 grid.arrange(ty_2018,ty_20xx, 
              top = "Confronto del numero di installazioni per data dell'ultimo aggiornamento", 
              left = textGrob("Frequenze",rot = 90, vjust = 0.5, hjust = 0.5))
@@ -74,5 +78,14 @@ grid.arrange(ty_2018,ty_20xx,
 #' decrescere il numero di installazioni, questo non avviene se l'app viene tenuta aggiornata. 
 #' In quest'ultimo caso infatti la curva tende ad avere un secondo picco più grande intorno al 
 #' milione di installazioni 
+
+
+
+# Rat
+ggplot(google_app) + geom_boxplot(aes(y=Rating, x=Installs, fill=Installs))
+ggplot(google_app) + geom_violin(aes(y=Rating, x=Installs, fill=Installs))
+
+
+ggplot(google_app) + geom_density(aes(x=Rating, col=Installs))
 
 ggplot(google_app[prova,]) + geom_point(aes(y=Price, x=Size, col= Installs)) + coord_cartesian(ylim = c(0,50))
